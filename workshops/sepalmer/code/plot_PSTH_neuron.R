015aabe0286474de64672cc6de788f2247717139
sepalmer <sepalmer@uchicago.edu>
2019-07-31 22:26:28 -0500

    Completed revisions of the neuro workshop.

--- workshops/sepalmer/code/plot_PSTH_neuron.R
+++ workshops/sepalmer/code/plot_PSTH_neuron.R
@@ -1,26 +1,33 @@
-plot_PSTH_neuron <- function(filename,filter_size){
+plot_PSTH_neuron <- function(filename,filter_size,specify_dirs=-1000){
   neur <- read_neuron(filename)
   unique_dirs <- neur[['udirs']]
   s_dirs <- neur[['dirs']]
   
+  if (specify_dirs != -1000) {
+    plot_dirs = specify_dirs;
+  }
+  else {
+    plot_dirs = unique_dirs
+  }
+  
   stim_onset <- 500
-  xs <- 1:2700 - stim_onset
-  conversion <- 1000
+  xs <- 1:2700 - stim_onset #set up the zero on the x-axis at stimulus onset
+  conversion <- 1000 #convert times from seconds to milliseconds
   
   # for a boxcar filter
   filter <- rep(1/filter_size, filter_size)
   smooth_raster <- stats::filter(neur[['spks']], filter)*conversion
   
-  mean_arr <- array(dim=c(length(unique_dirs), length(xs)))
-  for (i in 1:length(unique_dirs)) {
-    d <- unique_dirs[i]
+  mean_arr <- array(dim=c(length(plot_dirs), length(xs)))
+  for (i in 1:length(plot_dirs)) {
+    d <- plot_dirs[i]
     dir_raster <- smooth_raster[ , s_dirs == d]
     mean_arr[i, ] <- apply(dir_raster, 1, mean)
   }
   
   flat_means <- as.vector(t(mean_arr))
-  d = data.frame(ts=rep(xs, length(unique_dirs)),
-                 dir=rep(unique_dirs, length(unique_dirs), 
+  d = data.frame(ts=rep(xs, length(plot_dirs)),
+                 dir=rep(plot_dirs, length(plot_dirs), 
                          each=length(xs)),
                  spk_rates=flat_means)
   
